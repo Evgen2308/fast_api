19fb01e6da89277145bd4725924b90e53f1001ba
evgen2308 <evgen230881@gmail.com>
2023-10-16 00:32:00 +0200

    part 1-2

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
-
+from models import core
 from routers.items import router as items_router
+from models.database import engine
+from routers.users import router as users_router
+
+
+core.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(
     router=items_router,
     prefix="/items",
+)
+
+app.include_router(
+    router=users_router,
+    prefix="/users",
 )
